63cefbd9dc458f068e715b580e337c7a81f422d6
Piotr Kupisiewicz <deletek@ekg2.org>
2006-06-05 19:15:50 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.

--- plugins/perl/irc/module.h
+++
@@ -1,8 +0,0 @@
-#include "../common/module.h"
-
-#include "../../irc/misc.h"
-#include "../../irc/irc.h"
-
-typedef channel_t *Ekg2__Irc__Channel;
-typedef people_t  *Ekg2__Irc__User;
-typedef session_t  *Ekg2__Irc__Server;
